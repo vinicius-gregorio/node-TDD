3e270c34ec1d14ca8d1fca8df12661e6455f489e
Vinicius MAC <vincamgreg@hotmail.com>
2021-01-26 21:47:03 -0300

    test: ensure SignupController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignupController } from './signup'
+
+describe('Signup Controller ', () => {
+    test('Should return 400 if no name is provided ', () => {
+        const sut = new SignupController()
+        const httpRequest = { body : {
+           
+            email: 'any_email@email.com',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
+        }}
+       const httpResponse =  sut.handle(httpRequest)
+       expect(httpResponse.statusCode).toBe(400)
+    });
+});
