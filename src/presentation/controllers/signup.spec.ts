6b4721e90eea4afc3cf66bdf1caee2948ad83e0e
Vinicius MAC <vincamgreg@hotmail.com>
2021-01-26 21:57:47 -0300

    test: ensure SignupController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,4 +13,16 @@ describe('Signup Controller ', () => {
        expect(httpResponse.statusCode).toBe(400)
        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
     });
+    test('Should return 400 if no name is provided ', () => {
+        const sut = new SignupController()
+        const httpRequest = { body : {
+           
+            name: 'any name',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
+        }}
+       const httpResponse =  sut.handle(httpRequest)
+       expect(httpResponse.statusCode).toBe(400)
+       expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    });
 });
